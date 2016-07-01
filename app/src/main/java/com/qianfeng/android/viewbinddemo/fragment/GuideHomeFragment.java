e49b2b8e207e92b691c04ea760710543f245c5ad
孔阳 <kongyang717@163.com>
2016-07-01 18:13:08 +0800

    指南精选界面图片解析完成

--- app/src/main/java/com/qianfeng/android/viewbinddemo/fragment/GuideHomeFragment.java
+++ app/src/main/java/com/qianfeng/android/viewbinddemo/fragment/GuideHomeFragment.java
@@ -2,24 +2,30 @@ package com.qianfeng.android.viewbinddemo.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.qianfeng.android.viewbinddemo.R;
 import com.qianfeng.android.viewbinddemo.bean.GuideHomeContents;
+import com.qianfeng.android.viewbinddemo.utils.LogUtil;
 import com.qianfeng.android.viewbinddemo.utils.OkHttpUtil;
 import com.qianfeng.android.viewbinddemo.utils.URLConstant;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import butterknife.BindView;
@@ -33,6 +39,9 @@ public class GuideHomeFragment extends Fragment implements OkHttpUtil.CallBack {
     public static final int HEAD_VIEW = 0;
     public static final int ITEM_VIEW = 1;
 
+    public static final int HAS_TIME = 0;
+    public static final int NO_TIME = 1;
+
     /**
      * fragment 主控件，包含了整个界面的内容（头部View和List列表）
      */
@@ -40,6 +49,7 @@ public class GuideHomeFragment extends Fragment implements OkHttpUtil.CallBack {
     RecyclerView mRecyclerView;
 
     private List<GuideHomeContents.DataBean.ItemsBean> mListContents = new ArrayList<>();
+    private List<Integer> mListType = new ArrayList<>();
     private Context mContext;
     private MyAdapter mAdapter;
 
@@ -77,9 +87,37 @@ public class GuideHomeFragment extends Fragment implements OkHttpUtil.CallBack {
     @Override
     public void callback(String result) {
         GuideHomeContents bean = new Gson().fromJson(result, GuideHomeContents.class);
-
-        mListContents.addAll(bean.getData().getItems());
+        List<GuideHomeContents.DataBean.ItemsBean> list = bean.getData().getItems();
+        groupItem(list);
+        //刷新适配器
+        mListContents.addAll(list);
         mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * 根据时间为每个item数据分组
+     *
+     * @param list 数据列表
+     */
+    private void groupItem(List<GuideHomeContents.DataBean.ItemsBean> list) {
+        String currentTimeStr = "";
+        mListType.clear();
+        for (GuideHomeContents.DataBean.ItemsBean b : list) {
+            String time = getTimeString(b.getCreated_at());
+            if (time.equals(currentTimeStr)) {
+                mListType.add(NO_TIME);
+            } else {
+                mListType.add(HAS_TIME);
+            }
+            currentTimeStr = time;
+        }
+
+    }
+
+    private String getTimeString(long l) {
+        SimpleDateFormat sdf = new SimpleDateFormat("M月d日 E");
+        Date date = new Date(l * 1000);
+        return sdf.format(date);
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
@@ -87,6 +125,8 @@ public class GuideHomeFragment extends Fragment implements OkHttpUtil.CallBack {
         int viewType;
         @BindView(R.id.iv_guide_item_cover)
         ImageView mImageView;
+        @BindView(R.id.ll_guide_item_time)
+        LinearLayout mLlTime;
 
         public MyViewHolder(View itemView, int viewType) {
             super(itemView);
@@ -99,6 +139,13 @@ public class GuideHomeFragment extends Fragment implements OkHttpUtil.CallBack {
 
     class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
+        /**
+         * 此方法只会在第一次加载item的时候调用，后面会复用该Holder
+         *
+         * @param parent   父视图
+         * @param viewType view类型，RecyclerView会通过此类型判断新出现的item是否重用此view
+         * @return viewholder
+         */
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater from = LayoutInflater.from(mContext);
@@ -124,11 +171,18 @@ public class GuideHomeFragment extends Fragment implements OkHttpUtil.CallBack {
         @Override
         public void onBindViewHolder(MyViewHolder holder, int position) {
             GuideHomeContents.DataBean.ItemsBean info = mListContents.get(position);
+            //如果类型为有时间就显示时间布局
+            if (position > 0) {
+                if (mListType.get(position - 1) == HAS_TIME) {
+                    holder.mLlTime.setVisibility(View.VISIBLE);
+                } else {
+                    holder.mLlTime.setVisibility(View.GONE);
+                }
+            }
             if (holder.viewType == ITEM_VIEW) {
                 Picasso.with(mContext)
                         .load(info.getCover_image_url())
                         .into(holder.mImageView);
-
             }
         }
 
